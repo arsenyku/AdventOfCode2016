ccbd7b968f1156678c9645f7b228bec0a814d183
asu <arsenyk.ustaris.dev@gmail.com>
2016-12-15 19:34:12 -0800

    Day 6 Part 1

--- AdventOfCode2016/Day6.swift
+++ AdventOfCode2016/Day6.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+func testInput() -> [String]
+{
+  return [
+    "eedadn",
+    "drvtee",
+    "eandsr",
+    "raavrd",
+    "atevrs",
+    "tsrnev",
+    "sdttsa",
+    "rasrtv",
+    "nssdts",
+    "ntnada",
+    "svetve",
+    "tesnvt",
+    "vntsnd",
+    "vrdear",
+    "dvrsen",
+    "enarar"
+  ]
+}
+
+func day6()
+{
+  let pathAndFilename = basePath + "day6-input.txt"
+  let lines = readLines(pathAndFilename: pathAndFilename).filter({ !$0.isEmpty })
+  //let lines = testInput()
+  let messageLength = lines.first!.length
+  var modes = [String]()
+  
+  for c in 0..<messageLength
+  {
+    modes.append(lines.map({ $0[c]! }).mode!)
+  }
+  
+  print ("Day 6 Part 1 = \(modes.joined())")
+  
+}
