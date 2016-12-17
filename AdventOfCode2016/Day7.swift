3c603805cd0d98bb230ea0e67d7056f0577f8e15
asu <arsenyk.ustaris.dev@gmail.com>
2016-12-17 13:59:48 -0800

    Day 7 Part 1

--- AdventOfCode2016/Day7.swift
+++ AdventOfCode2016/Day7.swift
@@ -8,15 +8,38 @@
 
 import Foundation
 
+func supportsTLS(address:String) -> Bool
+{
+  let abbaPattern = "(([a-z])(?!\\2)([a-z])\\3\\2)"
+  let textInBracketsPattern = "\\[[^\\]]*\\]"
+
+  let hasAbba = address.firstMatch(ofPattern: abbaPattern) != nil
+  
+  if (!hasAbba)
+  {
+    return false
+  }
+ 
+  let textInBrackets = address.matches(ofPattern: textInBracketsPattern)
+  
+  for matchedText in textInBrackets
+  {
+    let hasAbbaInBrackets = matchedText.firstMatch(ofPattern: abbaPattern) != nil
+    
+    if (hasAbbaInBrackets)
+    {
+      return false
+    }
+  }
+  
+  return true
+}
 
 func day7()
 {
   let pathAndFilename = basePath + "day7-input.txt"
   let lines = readLines(pathAndFilename: pathAndFilename)
-
+  let result1 = lines.filter({ supportsTLS(address: $0) }).count
   
-  for line in lines
-  {
-//    print (line)
-  }
+  print ("Day 7 Part 1 = \(result1)")
 }
