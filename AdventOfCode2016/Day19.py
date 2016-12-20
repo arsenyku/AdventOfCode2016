c50ad73f7ff33f13506be2d17894e89cf9ce2c2b
asu <arsenyk.ustaris.dev@gmail.com>
2016-12-20 01:08:42 -0800

    Correct version of python script that solved day 19 part 2

--- AdventOfCode2016/Day19.py
+++ AdventOfCode2016/Day19.py
@@ -1,6 +1,6 @@
 
 def whiteElephantV2(numberOfElves):
-  elvesWithGifts = range(numberOfElves)
+  elvesWithGifts = range(1,numberOfElves+1)
   stillPlaying = len(elvesWithGifts)
   stealer = 0
 
@@ -9,21 +9,35 @@ def whiteElephantV2(numberOfElves):
   while (stillPlaying > 1):
     stealee = ((stillPlaying/2) + stealer) % stillPlaying
 
+    if (stillPlaying > 1 and stillPlaying < 20):
+      print elvesWithGifts, elvesWithGifts[stealer], elvesWithGifts[stealee]
+
     #elvesWithGifts.remove(at: stealee)
     del elvesWithGifts[stealee]
     stillPlaying -= 1
     
-    stealer = 0 if (stealer >= stillPlaying) else  ((stealer+1) % stillPlaying)
-
-    if (iterations % 10000 == 0):
-      print "%d iterations" % iterations
+    #stealer = 0 if (stealer >= stillPlaying) else  ((stealer+1) % stillPlaying)
+    #stealer = 0 if (stealer >= stillPlaying) else  ((stealer+1) % stillPlaying)
+    if (stealer >= stillPlaying):
+      stealer = 0
+    elif (stealee > stealer):
+      stealer = ((stealer+1) % stillPlaying)
+    #else 
+    #  no change to stealer
 
     iterations += 1
-  
+    if (iterations % 10000 == 0):
+      print "%d iterations, %d stillPlaying, %d len" % (iterations, stillPlaying, len(elvesWithGifts))
+
+  #print elvesWithGifts, elvesWithGifts[stealer], elvesWithGifts[stealee]
+
   return elvesWithGifts[0]
 
 if __name__ == '__main__':
-  numberOfElves = 3001330;
+  numberOfElves = 3001330
+  #numberOfElves = 100
+
   result = whiteElephantV2( numberOfElves )
   
   print result
+
